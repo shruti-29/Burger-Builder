da329a4d7245eeba941cef5669e8fb6b52bc4920
Shruti Shrivastava <shruti29shrivastav@gmail.com>
2021-04-18 18:49:31 +0530

    remove ingredients dynamically

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,12 +33,39 @@ class BurgerBuilder extends Component{
         this.setState({totalPrice: newPrice,ingredients: updatedIngredients});
     };
 
+    removeIngredientsHandler = (type) => {
+        const currentCount = this.state.ingredients[type];
+        if(currentCount <= 0){
+            return;
+        }
+        const updatedCount = currentCount - 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount;
+        const priceDecrease = INGREDIENT_PRICES[type];
+        const newPrice = this.state.totalPrice - priceDecrease;
+        this.setState({totalPrice: newPrice,ingredients: updatedIngredients});
+
+    };
+
 
         render(){
+            const disabledInfo = {
+                ...this.state.ingredients
+            };
+            for(let key in disabledInfo){
+                disabledInfo[key] = disabledInfo[key] <=0
+            }
+            
         return(
             <Aux>
                <Burger ingredients={this.state.ingredients}/>  
-               <BuildControls ingredientsAdded= {this.addIngredientHandler}/>
+               <BuildControls 
+               ingredientsRemoved = {this.removeIngredientsHandler}
+               ingredientsAdded= {this.addIngredientHandler}
+            disabled={disabledInfo}
+               />
             </Aux>
         )
     };

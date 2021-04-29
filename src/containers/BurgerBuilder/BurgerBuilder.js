ec284d5c3c093236f404b647f739b5420fea5e2d
Shruti Shrivastava <shruti29shrivastav@gmail.com>
2021-04-29 23:23:21 +0530

    modified checkout page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -104,7 +104,16 @@ class BurgerBuilder extends Component{
         //         this.setState({loading:false,purchasing:false});
         //         console.log(error);
         //     })
-        this.props.history.push('/checkout')
+
+        const queryParams = [];
+        for(let i in this.state.ingredients){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+        }
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname : '/checkout',
+            search: '?' + queryString
+        })
         //to push checkout page onto the stack
         
     };
